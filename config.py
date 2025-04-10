4c24b48e78d1581a610ab76f451a0dec56b24b28
belmone-carteirinha <clariny1@hotmail.com>
2025-04-10 18:21:33 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# app/config.py
-
-import os
-
-class Config:
-    SECRET_KEY = 'sua_chave_secreta'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///carteirinhas.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
